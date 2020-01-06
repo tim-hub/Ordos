fa56193ddc02ce39769b752ec201991f260d35ae
tim-hub <tim_w@outlook.com>
2020-01-06 22:17:12 +1300

    require more tests for site

--- generator/tests/test_site.py
+++ generator/tests/test_site.py
@@ -7,11 +7,16 @@ from utils.io import get_all_sites
 
 class SiteTestCase(unittest.TestCase):
     sites = list(map(lambda s: Site(s['path'], s['name']), get_all_sites(the_path)))
+    site = sites[0]
 
     def test_sites(self):
-        self.sites[0].save()
+        self.site.save()
         # serve(self.sites[0].output_dir)
-        return
+
+    def test_site_name(self):
+        self.assertEqual(self.site.name, 'tim-hub.github.io')
+
+
 
 
 if __name__ == '__main__':
